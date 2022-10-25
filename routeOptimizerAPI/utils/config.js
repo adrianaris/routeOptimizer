e09b21195aeac872a4587be5cd8c7f52e1de4ed4
Adrian Serbanescu <adrian@adrianserbanescu.com>
2022-10-25 16:28:56 +0200

    remove server from the repo and frontface the client

--- routeOptimizerAPI/utils/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN
-const PORT = process.env.PORT
-const GEOAPIFY_TOKEN = process.env.GEOAPIFY_TOKEN
-const MONGODB_URI = process.env.MONGODB_URI
-
-module.exports = { MAPBOX_TOKEN, PORT, GEOAPIFY_TOKEN, MONGODB_URI }
